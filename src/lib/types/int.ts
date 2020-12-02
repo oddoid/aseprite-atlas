c7cf7b2ad0e4a48d211c5bdcb9b33cb0e5ea497d
Stephen Niedzielski <stephen@niedzielski.com>
2020-12-01 20:57:22 -0700

    [int] Refactor functions to use tryTo()

--- src/lib/types/int.ts
+++ src/lib/types/int.ts
@@ -1,21 +1,32 @@
-/** An integral number. */
+/**
+ * An integer `number` in
+ * [`Number.MIN_SAFE_INTEGER`, `Number.MAX_SAFE_INTEGER`].
+ */
 export type Int = number & {[brand]: void}
 declare const brand: unique symbol
 
 export function Int(value: number): Int {
-  if (Number.isNaN(value)) throw new Error('Value is NaN.')
-  if (value > Number.MAX_SAFE_INTEGER) return <Int>Number.MAX_SAFE_INTEGER
-  if (value < Number.MIN_SAFE_INTEGER) return <Int>Number.MIN_SAFE_INTEGER
-  return <Int>Math.trunc(value)
+  const int = Int.tryTo(value)
+  if (int === undefined)
+    throw new Error(`${value} cannot be converted to an Int.`)
+  return int
 }
 
+// This can be made more generic as a constrained RangeInt / DomainInt / ...
 export namespace Int {
+  /** Truncate with saturation. */
+  export function tryTo(value: number): Int | undefined {
+    if (Number.isNaN(value)) return
+    if (value > Number.MAX_SAFE_INTEGER) return <Int>Number.MAX_SAFE_INTEGER
+    if (value < Number.MIN_SAFE_INTEGER) return <Int>Number.MIN_SAFE_INTEGER
+    return <Int>Math.trunc(value)
+  }
+
   export function assert(value: number | Int): asserts value is Int {
     if (!is(value)) throw new Error(`${value} is not an Int.`)
   }
 
   export function is(value: number | Int): value is Int {
-    if (Number.isNaN(value)) return false
-    return Int(value) === value
+    return tryTo(value) === value
   }
 }
